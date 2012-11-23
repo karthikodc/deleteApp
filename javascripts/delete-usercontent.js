c612221c88874f23c0b1d2a37eec910a6c243710
Karthik Raja <k.raja@accenture.com>
2012-11-23 17:08:36 +0530

    commit

--- javascripts/delete-usercontent.js
+++ javascripts/delete-usercontent.js
@@ -1,29 +1,30 @@
 
-
-$(document).ready(function () {
-  $("#content").text("The DOM is now loaded and can be manipulated.");
-	var viewer;
-	console.log("Ready started");
-	
-	function init() {
-		loadUser();
-		viewer = opensocial.data.getDataContext().getDataSet('viewer');
-		console.log("loadUser() started");
+function init() {
+	loadUser();
+	viewer = opensocial.data.getDataContext().getDataSet('viewer');
+	console.log("loadUser() started");
 		
-	};
-		function loadUser() {
-		console.log("loadUser() started");
-		showMessage("Loading the currently logged in user ...");
-		osapi.jive.core.users.get({
-			id : '@viewer'
-		}).execute(function(response) {
-				console.log("loadUser() response = " + JSON.stringify(response));
-				user = response.data;
-				$(".user-name").html("").html(user.name);
-				loadGroups();
-			});
-	}
-});
+};
+
+	
+function loadUser() {
+	$(document).ready(function () {
+		$("#content").text("The DOM is now loaded and can be manipulated.");
+		var viewer;
+		console.log("Ready started");
+	});
+	console.log("loadUser() started");
+	showMessage("Loading the currently logged in user ...");
+	osapi.jive.core.users.get({
+		id : '@viewer'
+	}).execute(function(response) {
+			console.log("loadUser() response = " + JSON.stringify(response));
+			user = response.data;
+			$(".user-name").html("").html(user.name);
+			loadGroups();
+		});
+}
+
 
 // Register our on-view-load handler
 gadgets.util.registerOnLoadHandler(init);
