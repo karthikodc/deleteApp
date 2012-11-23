4b5d57aa315836607a59c3c77944dd0cf050a3ea
Karthik Raja <k.raja@accenture.com>
2012-11-23 15:54:37 +0530

    commit

--- javascripts/delete-usercontent.js
+++ javascripts/delete-usercontent.js
@@ -2,4 +2,17 @@
 
 $(document).ready(function () {
   $("#content").text("The DOM is now loaded and can be manipulated.");
+	var documentURI = ""; //A given URI of a document
+	var authorURI="https://apps-onprem.jivesoftware.com/people/"
+	var placeURI="https://apps-onprem.jivesoftware.com/groups/accenturetest";
+	var request = osapi.jive.corev3.contents.get({
+     "type": "document",
+     "fields": "@all"
+	 "place":placeURI,
+	 "author":authorURI,
+	});
+
+	request.execute(function(data) {
+     console.log("Fetched the document!", data);
+	});
 });
