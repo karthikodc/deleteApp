50ff2baaf5e3852e7046b6668c996aac2d78433d
Karthik Raja <k.raja@accenture.com>
2012-11-23 14:40:10 +0530

    commit

--- javascripts/delete-usercontent.js
+++ javascripts/delete-usercontent.js
@@ -1,19 +1,5 @@
 
 
-// On-view-load initialization
-function init() {
-      $(document).ready(getUserContent);
-	  gadgets.window.adjustHeight();
-}
-
-
 $(document).ready(function () {
-  
-
-function getUserContent() {
-$("#content").text("The DOM is now loaded and can be manipulated.");
+  $("#content").text("The DOM is now loaded and can be manipulated.");
 });
-}
-
-// Register our on-view-load handler
-gadgets.util.registerOnLoadHandler(init);
